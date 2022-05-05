6f97276ee83138b6555e5e4b5a34dc513699d57e
ReitoSerizawa <s.reitiger@gmail.com>
2022-05-05 09:55:52 -0400

    commit tree 2c5abe0c83c1c9ea5f0fa6ba8a60ea2eae3223f7

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, {useState, useEffect} from "react"
+
+function App() {
+
+    const [dataLoaded, setData] = useState(false)
+
+    useEffect(() => {
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(r => r.json())
+        .then(data => setData(data))}
+        ,[]
+    )
+    
+    return (
+        <div>
+        {dataLoaded? <img src={dataLoaded.message} alt="A Random Dog"></img> : <p>Loading</p>}
+        </div>
+    )
+
+}
+
+export default App
